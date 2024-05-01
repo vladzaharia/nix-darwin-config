248ff4b7646d51e136539e15bf5cfa176aa90d05
Vlad Zaharia <v.zaharia@pokemon.com>
2024-04-30 19:33:21 -0700

    Add work mac

--- flake.nix
+++ flake.nix
@@ -36,6 +36,20 @@
           ./src/config/user.nix
         ];
       };
+      darwinConfigurations."Mac-6LV21Y21X" = nix-darwin.lib.darwinSystem {
+        specialArgs =
+          inputs
+          // {
+            username = "v.zaharia";
+          };
+
+        modules = [
+          ./src/config/nix-darwin.nix
+          ./src/config/aarch64.nix
+          ./src/config/pkgs.nix
+          ./src/config/user.nix
+        ];
+      };
 
       # Expose the package set, including overlays, for convenience.
       darwinPackages = self.darwinConfigurations.default.pkgs;
